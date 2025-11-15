1da6fbe1ca9a0d71017fa343a46286b2f95ae1ec
vanessallfeng <l6feng@ucsd.edu>
2025-11-14 21:28:18 -0800

    fixes with pink, boston + cambridge

--- map.js
+++ map.js
@@ -13,36 +13,45 @@ const map = new mapboxgl.Map({
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
 });
+
+// Wait for the map to load before adding data
 map.on('load', async () => {
+  // Boston bike lanes source
   map.addSource('boston_route', {
     type: 'geojson',
     data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
   });
 
+  // Boston bike lanes layer
   map.addLayer({
     id: 'bike-lanes',
     type: 'line',
     source: 'boston_route',
     paint: {
-      'line-color': '#ff69b4',
+      'line-color': '#ff69b4', // hot pink
+      'line-width': 5,
+      'line-opacity': 0.6,
+    },
+  });
+
+  // Cambridge bike lanes source
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://cambridgegis.github.io/gisdata/bike-lanes.geojson',
+  });
+
+  // Cambridge bike lanes layer
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: {
+      'line-color': '#ff69b4', // hot pink to match
       'line-width': 5,
       'line-opacity': 0.6,
     },
   });
 });
-
-map.addSource('cambridge_route', {
-  type: 'geojson',
-  data: 'https://cambridgegis.github.io/gisdata/bike-lanes.geojson',
-});
-map.addLayer({
-    id: 'cambridge-bike-lanes',
-    type: 'line',
-    source: 'cambridge_route',
-    paint: {
-      'line-color': '#ff69b4',
-      'line-width': 5,
-      'line-opacity': 0.6,
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
