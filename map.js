adbf9d0651bf4443b87940b16e1246d1538581ae
vanessallfeng <l6feng@ucsd.edu>
2025-11-14 20:56:26 -0800

    mapbox access token

--- map.js
+++ map.js
@@ -1,8 +1,18 @@
-// TEMP: make sure map.js is running at all
-console.log('map.js is running');
-
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1IjoidmFuZXNzYWxmZW5nIiwiYSI6ImNtaHpwNDBlcjBjb2cyam9najdmMHdieTMifQ.cGOsnvL8OINWs416eljWLA';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
