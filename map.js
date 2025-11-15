37a1acb624e50332800dfca3d2e68f8c4147db34
vanessallfeng <l6feng@ucsd.edu>
2025-11-14 20:17:18 -0800

    debug map.js

--- map.js
+++ map.js
@@ -1,4 +1,8 @@
+// TEMP: make sure map.js is running at all
+console.log('map.js is running');
+
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
